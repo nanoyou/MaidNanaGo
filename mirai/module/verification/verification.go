e44e31845900da317eddacfeb8507cecd05bac23
PermissionDog <455662503@qq.com>
2022-09-17 14:04:49 +0800

    feat: 添加调试日志

--- mirai/module/verification/verification.go
+++ mirai/module/verification/verification.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Mrs4s/MiraiGo/client"
 	"github.com/Mrs4s/MiraiGo/message"
 	"github.com/nanoyou/MaidNanaGo/service"
+	"github.com/sirupsen/logrus"
 )
 
 var instance *Verification
@@ -44,11 +45,14 @@ func (v *Verification) Serve(b *bot.Bot) {
 	b.OnPrivateMessageF(func(pm *message.PrivateMessage) bool {
 		return strings.TrimSpace(pm.ToString()) == "验证码"
 	}, func(q *client.QQClient, pm *message.PrivateMessage) {
+		logrus.Debug("尝试获取验证码")
 		code, err := service.GetUserService().CreateVerificationCode(pm.Sender.Uin)
 		if err != nil {
+			logrus.WithError(err).Debug("获取失败")
 			b.SendPrivateMessage(pm.Sender.Uin, message.NewSendingMessage().Append(message.NewText("获取失败: "+err.Error())))
 			return
 		}
+		logrus.WithField("code", code).Debug("获取成功")
 		b.SendPrivateMessage(pm.Sender.Uin, message.NewSendingMessage().Append(message.NewText(fmt.Sprintf("您的验证码是: %v", code))))
 	})
 }
