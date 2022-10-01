85edadb624586dd760b4f5edf8ca46fca6494602
PermissionDog <455662503@qq.com>
2022-10-02 00:02:40 +0800

    fix: 管理员修改用户信息校验问题

--- controller/request/admin.go
+++ controller/request/admin.go
@@ -1,6 +1,6 @@
 package request
 
 type AdminModifyUserRequest struct {
-	Username string `json:"username" validate:"max=20,urlsafename"`
-	Password string `json:"password" validate:"max=32"`
+	Username string `json:"username" validate:"min=4,max=20,urlsafename"`
+	Password string `json:"password" validate:"min=6,max=32"`
 }
