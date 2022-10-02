5ed44710d79e16d511a7b6c3493caf95de09b2d1
PermissionDog <455662503@qq.com>
2022-10-02 22:50:02 +0800

    refactor: 修改模板请求

--- controller/request/announcement.go
+++ controller/request/announcement.go
@@ -3,13 +3,13 @@ package request
 import "github.com/nanoyou/MaidNanaGo/model"
 
 type CreateTemplateRequest struct {
-	Visibility *model.VisibilityType `validate:"required,visibility"`
-	Name       string                `validate:"required,max=20"`
-	Content    string                `validate:"max=1000"`
+	Visibility model.VisibilityType `validate:"required,visibility"`
+	Name       string               `validate:"required,max=20"`
+	Content    string               `validate:"max=1000"`
 }
 
 type ModifyTemplateRequest struct {
-	Visibility *model.VisibilityType `validate:"visibility"`
-	Name       string                `validate:"max=20"`
-	Content    string                `validate:"max=1000"`
+	Visibility model.VisibilityType `validate:"len=0|visibility"`
+	Name       string               `validate:"max=20"`
+	Content    string               `validate:"max=1000"`
 }
