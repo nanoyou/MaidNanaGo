f9fb67edb610589085b2f1d0c467ee02299924af
AkagawaTsurunaki <965145619@qq.com>
2022-10-05 23:35:10 +0800

    feat: 数据类型中的私聊消息

--- mirai/types.go
+++ mirai/types.go
@@ -19,16 +19,23 @@ const (
 // 消息上报
 type MessageEvent struct {
 	Event
-	SubType   SubType `json:"sub_type"`
-	MessageID int32   `json:"message_id"`
-	UserID    int64   `json:"user_id"`
-	// TODO: message是个啥
-	Message    any    `json:"message"`
-	RawMessage string `json:"raw_message"`
-	Font       int    `json:"font"`
-	// TODO: implements
-	Sender MessageSender `json:"sender"`
+	SubType    SubType       `json:"sub_type"`
+	MessageID  int32         `json:"message_id"`
+	UserID     int64         `json:"user_id"`
+	Message    Message       `json:"message"`
+	RawMessage string        `json:"raw_message"`
+	Font       int           `json:"font"`
+	Sender     MessageSender `json:"sender"`
 }
+
+// TODO: message是个啥
+type Message any
+
+// TODO: implements
+// 需要注意的是, sender 中的各字段是尽最大努力提供的,
+// 也就是说, 不保证每个字段都一定存在,
+// 也不保证存在的字段都是完全正确的(缓存可能过期).
+type MessageSender any
 
 // https://docs.go-cqhttp.org/event/#%E6%B6%88%E6%81%AF%E4%B8%8A%E6%8A%A5
 
@@ -60,5 +67,18 @@ type MetaEventType struct {
 	MetaEventType PostType `json:"meta_event_type"`
 }
 
+// 私聊消息
 type PrivateMessage struct {
+	Event
+
+	// 事件数据
+	MessageType  MessageType  `json:"message_type"`
+	MessageEvent MessageEvent `json:"message_event"`
+	TempSource   int          `json:"temp_source"`
+
+	// 快速操作
+	Reply      Message `json:"reply"`
+	AutoEscape bool    `json:"auto_escape"`
 }
+
+type MessageType string
