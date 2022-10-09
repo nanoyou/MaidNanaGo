fd3f0450849664dfa3c6530934ec42b302ac5fcc
PermissionDog <455662503@qq.com>
2022-10-09 17:01:21 +0800

    feat: 公告model

--- model/announcement.go
+++ model/announcement.go
@@ -1,9 +1,11 @@
 package model
 
+import "gorm.io/gorm/clause"
+
 type Template struct {
 	VisibleModel
-	Content string
-	Name    string
+	Content string `gorm:"not null"`
+	Name    string `gorm:"not null"`
 }
 
 // Create 写入数据库
@@ -43,12 +45,12 @@ const (
 type Announcement struct {
 	BaseModel
 	Visibility VisibilityType
-	Owner      User `gorm:"foreignKey:OwnerID"`
-	OwnerID    uint
-	Name       string
-	Type       AnnouncementType
+	Owner      User             `gorm:"foreignKey:OwnerID"`
+	OwnerID    uint             `gorm:"not null"`
+	Name       string           `gorm:"not null"`
+	Type       AnnouncementType `gorm:"not null"`
 	Groups     []AnnouncementGroup
-	Enabled    Bool
+	Enabled    Bool `gorm:"not null"`
 	Crons      []AnnouncementCron
 	Variables  []AnnouncementVariable
 	Content    string   // 如为纯文本公告则包含此项
@@ -58,19 +60,100 @@ type Announcement struct {
 
 type AnnouncementGroup struct {
 	BaseModel
-	Group          int64
-	AnnouncementID uint
+	Group          int64 `gorm:"uniqueIndex:group; not null"`
+	AnnouncementID uint  `gorm:"uniqueIndex:group; not null"`
 }
 
 type AnnouncementCron struct {
 	BaseModel
-	Cron           string
-	AnnouncementID uint
+	Cron           string `gorm:"not null"`
+	AnnouncementID uint   `gorm:"not null"`
 }
 
 type AnnouncementVariable struct {
 	BaseModel
-	Key            string
+	Key            string `gorm:"uniqueIndex:key; not null"`
 	Value          string
-	AnnouncementID uint
+	AnnouncementID uint `gorm:"uniqueIndex:key; not null"`
+}
+
+// Create 写入数据库
+func (a *Announcement) Create() error {
+	return db.Create(&a).Error
+}
+
+// GetAnnouncementById 使用id获取公告
+func GetAnnouncementById(id uint) (a *Announcement, err error) {
+	err = db.Preload("Owner").Preload("Groups").
+		Preload("Crons").
+		Preload("Variables").
+		Preload("Template").First(&a, id).Error
+	return
+}
+
+// GetAllAnnouncements 获取所有公告
+func GetAllAnnouncements() (announcements []Announcement, err error) {
+	err = db.Model(&Template{}).
+		Preload("Owner").
+		Preload("Groups").
+		Preload("Crons").
+		Preload("Variables").
+		Preload("Template").Find(&announcements).Error
+	return
+}
+
+// Update 将更改存入数据库
+func (a *Announcement) Update() error {
+	return db.Updates(&a).Error
+}
+
+// Delete 删除公告
+func (a *Announcement) Delete() error {
+	return db.Select("Groups").Select("Crons").Select("Variables").Delete(&a).Error
+}
+
+// AddGroup 添加QQ群
+func (a *Announcement) AddGroup(group int64) error {
+	return db.Clauses(&clause.OnConflict{UpdateAll: true}).Create(&AnnouncementGroup{
+		AnnouncementID: a.ID,
+		Group:          group,
+	}).Error
+}
+
+// DeleteGroup 删除QQ群
+func (a *Announcement) DeleteGroup(group int64) error {
+	return db.Delete(&AnnouncementGroup{
+		AnnouncementID: a.ID,
+		Group:          group,
+	}).Error
+}
+
+// AddCron 添加Cron表达式
+func (a *Announcement) AddCron(cron string) error {
+	return db.Create(&AnnouncementCron{
+		AnnouncementID: a.ID,
+		Cron:           cron,
+	}).Error
+}
+
+// DeleteCron 通过cron表达式id删除Cron表达式
+func (a *Announcement) DeleteCron(cronId uint) error {
+	return db.Delete(&AnnouncementCron{}, cronId).Error
+}
+
+// SetVariable 设置变量
+func (a *Announcement) SetVariable(key string, value string) error {
+	return db.Clauses(&clause.OnConflict{UpdateAll: true}).Create(&AnnouncementVariable{
+		AnnouncementID: a.ID,
+		Key:            key,
+		Value:          value,
+	}).Error
+}
+
+// UnsetVariable 删除变量
+func (a *Announcement) UnsetVariable(key string) error {
+	return db.Delete(&AnnouncementVariable{
+		AnnouncementID: a.ID,
+		Key:            key,
+	}).Error
 }
